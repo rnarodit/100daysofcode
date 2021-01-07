5834c64c569856b721febdd52283bb26817b3864
Ron Naroditsky <ron@naroditsky.org>
2021-01-06 20:33:24 -0500

    commit

--- Day1/variable.py
+++ Day1/variable.py
@@ -1,5 +1,9 @@
 #veriable lets you store data for later use in the code. can refer to that data by the variable name
 #data in the variable can be changed
+#make your code readable, use veriable names that make sense
+#numbers cannot be in the begining of the variable
+#dont use the name of functions such as print or input as a variable name
+#use underscore to have variable that consists of multiple words
 name = "Jack"
 print(name)
 
