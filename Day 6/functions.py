5369ee22a697cf983ec1dc3d602b43ad071c4e2e
Ron Naroditsky <ron@naroditsky.org>
2021-01-13 20:22:02 -0500

    day 7

---
+++ Day 6/functions.py
@@ -0,0 +1,11 @@
+print() # parenthesis () -> tells u somehting is a fucnction , you put the input into the function in the parenthesis. function sometime can have no input
+num_char = len ("Hello")
+print(num_char)
+
+ # creating a function
+def my_function ():
+    print("Hello")
+print ("Bye")
+
+#need to call the function to use it
+my_function () 
---
+++ Day 6/whileLoop.py
@@ -0,0 +1,10 @@
+#while something_is_true :  when something becomes false the loop stops
+    #do something
+
+number_of_hurdles =6 
+while number_of_hurdles > 0 :
+    number_of_hurdles -= 1
+    print(number_of_hurdles)
+
+#use for loops if you need to do something to each thin you are iterating over for ex a list . use while loop when u want to carry out a functionallity until a condition is no longer true
+# while loops are more dangerous bc you can create an infinite loop
